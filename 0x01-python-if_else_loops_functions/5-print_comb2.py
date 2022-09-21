9a213f94062449e39d1e7b0ad30226c0b4ddbc13
Mark Bonchi <markbonchi@gmail.com>
2022-09-21 08:02:35 -0700

    Python script

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    if i < 10:
-        print("0{0}".format(i), end= ', ')
-    elif i != 99:
-        print("{0}".format(i), end= ', ')
-    else:
+    if i == 99:
         print(i)
+        break
+    print('{:0>2}'.format(i), end=", ")
