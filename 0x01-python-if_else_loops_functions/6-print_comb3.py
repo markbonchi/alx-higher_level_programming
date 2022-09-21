5c7a40bae00a2fc717f893f89924fea10527e69c
Mark Bonchi <markbonchi@gmail.com>
2022-09-21 08:14:51 -0700

    Python script

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(9):
     for j in range(1, 10):
         if j > i:
             if i == 8 and j == 9:
-                print('{0}{1}'.format(i,j))
+                print('{0}{1}'.format(i, j))
                 break
-            print("{0}{1}".format(i,j), end=', ')
+            print("{0}{1}".format(i, j), end=', ')
