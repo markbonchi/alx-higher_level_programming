f3b284813bb4bfb5f9b5db33cc59024d6bbbf4ab
Mark Bonchi <markbonchi@gmail.com>
2022-09-22 16:40:37 -0700

    Python script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = 1;
+    length = len(sys.argv)
+    if length > 1:
+        print("{} arguments:".format(length - 1))
+        for item in sys.argv[1:]:
+            print("{}: {}".format(count, item))
+            count += 1
+    else:
+        print("{} arguments.".format(length - 1))
