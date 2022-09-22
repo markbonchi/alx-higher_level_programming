e15e19669b2fdc28c3df4064d6c70363981d9157
Mark Bonchi <markbonchi@gmail.com>
2022-09-22 13:40:50 -0700

    New directory

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{0:d} + {1:d} = {2:d}".format(a, b, calc.add(a, b)))
     print("{0:d} + {1:d} = {2:d}".format(a, b, calc.sub(a, b)))
     print("{0:d} + {1:d} = {2:d}".format(a, b, calc.mul(a, b)))
-    print("{0} + {1} = {2}".format(a, b, calc.div(a, b)))
+    print("{0:d} + {1:d} = {2:d}".format(a, b, calc.div(a, b)))
