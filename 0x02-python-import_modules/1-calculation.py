9e4d74dd377bd3be3f816de472e5379cd5b7db47
Mark Bonchi <markbonchi@gmail.com>
2022-09-22 14:05:09 -0700

    Python script

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{0:d} + {1:d} = {2:d}".format(a, b, calc.add(a, b)))
-    print("{0:d} + {1:d} = {2:d}".format(a, b, calc.sub(a, b)))
-    print("{0:d} + {1:d} = {2:d}".format(a, b, calc.mul(a, b)))
-    print("{0:d} + {1:d} = {2}".format(a, b, calc.div(a, b)))
+    print("{0:d} - {1:d} = {2:d}".format(a, b, calc.sub(a, b)))
+    print("{0:d} * {1:d} = {2:d}".format(a, b, calc.mul(a, b)))
+    print("{0:d} / {1:d} = {2:d}".format(a, b, calc.div(a, b)i))
