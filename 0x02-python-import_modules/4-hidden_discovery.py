89aa6585504658bedb13357b2f24e29f22391d64
Mark Bonchi <markbonchi@gmail.com>
2022-09-22 18:05:34 -0700

    Python script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    this_list = dir(hidden_4)
-    for item in this_list.sort():
-        if item[0] != "_" and item[1] != "_":
+    for item in dir(hidden_4):
+        if item[0] != '_' and item[1] != '_':
             print(item)
