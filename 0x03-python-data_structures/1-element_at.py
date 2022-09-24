98b7bfe39d9da516e9c96a13eef08e59c18e250b
Mark Bonchi <markbonchi@gmail.com>
2022-09-24 11:02:03 -0700

    Secure access to element in list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return 'None'
+    for item in my_list:
+        if item == my_list[idx]:
+            return item
+
