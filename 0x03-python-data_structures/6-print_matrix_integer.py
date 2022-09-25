a9f40630dfa026887912a9e849cbe76359965376
Mark Bonchi <markbonchi@gmail.com>
2022-09-25 06:28:00 -0700

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for item in list:
-            print("{}".format(item), end=' ')
+            print("{:d}".format(item), end=' ')
         print()
