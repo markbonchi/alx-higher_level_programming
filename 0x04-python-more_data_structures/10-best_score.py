9211dc29539e4a1cf60e97213efddc26ec3ed2cb
Mark Bonchi <markbonchi@gmail.com>
2022-09-30 15:55:58 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
     count = 0
     for student, score in a_dictionary.items():
