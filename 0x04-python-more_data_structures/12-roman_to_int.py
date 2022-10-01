45a724b069734463745b7dc0961edaacf96b1b98
Mark Bonchi <markbonchi@gmail.com>
2022-09-30 17:14:46 -0700

    Roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    number = 0
+    for i in roman_string:
+        if i == 'I':
+            number += 1
+        elif i == 'V':
+            number += 5
+        elif i == 'X':
+            number += 10
+        elif i == 'L':
+            number += 50
+        elif i == 'C':
+            number += 100
+        elif i == 'D':
+            number += 500
+        elif i == 'M':
+            number += 1000
+        else:
+            return None
+
+    if 'IV' or 'IX' in roman_string:
+        number -= 2 * roman_string.count('IV')
+        number -= 2 * roman_string.count('IX')
+    elif 'XL' or 'XC' in roman_string:
+        number -= 20 * roman_string.count('XL')
+        number -= 20 * roman_string.count('XC')
+    elif 'CD' or 'CM' in roman_string:
+        number -= 200 * roman_string.count('CD')
+        number -= 200 * roman_string.count('CM')
+    return number
