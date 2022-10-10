3159f422946ded433281f40e22d0f12a3252d726
Mark Bonchi <markbonchi@gmail.com>
2022-10-10 12:28:46 -0700

    Safe print integer Error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        print("{:d}".format(value))
+    except Exception as err:
+        print("Exception: {}".format(err), file=stderr)
+        return False
+    else:
+        return True
