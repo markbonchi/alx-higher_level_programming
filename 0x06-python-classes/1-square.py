f03d4de6199b147f1c5b9c74d535d70772cb09eb
Mark Bonchi <markbonchi@gmail.com>
2022-10-11 15:38:15 -0700

    Initialize

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    __init__ method for parameters of square
+    args:
+        param1 (int): describe the size in units
+    '''
+    def __init__(self, size):
+        self.__size = size
