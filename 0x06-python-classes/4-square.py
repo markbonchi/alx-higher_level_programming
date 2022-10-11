44e2aed23da147842afa97e6e8ad12dde1397ed3
Mark Bonchi <markbonchi@gmail.com>
2022-10-11 16:21:54 -0700

    Initialize getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -33,7 +33,12 @@ class Square:
         args:
             param1 (int): size
         '''
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
         '''
