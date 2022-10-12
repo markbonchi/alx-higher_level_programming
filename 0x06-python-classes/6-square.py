334c2ff90a294480798ef759485ad0bde8935960
Mark Bonchi <markbonchi@gmail.com>
2022-10-11 17:15:29 -0700

    Print Squarex

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,7 +10,7 @@ class Square:
     args:
         param1 (int): size
     '''
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0,0)):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -18,6 +18,19 @@ class Square:
         else:
             self.__size = size
 
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) and type(position[1]) is not int
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    @property
+    def position(self):
+        '''Return (tuple): position'''
+        return self.__position
     @property
     def size(self):
         '''
@@ -25,20 +38,32 @@ class Square:
         '''
         return self.__size
 
+    @position.setter
+    def position(self, value):
+        ''''''
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) and type(position[1]) is not int
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     @size.setter
-    def size(self, size):
+    def size(self, value):
         '''
         Set value for private attribute size
 
         args:
             param1 (int): size
         '''
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         '''
