b8de4ec0dc1aac2478b8cc6e6e04ce96066eeef6
Mark Bonchi <markbonchi@gmail.com>
2022-10-17 20:39:17 -0700

    Square

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -63,10 +63,6 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         '''creates a square instance'''
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         return cls(size, size)
 
     @staticmethod
