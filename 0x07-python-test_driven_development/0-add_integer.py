54859636b4fc246192a9cf8cf439cac9cbf59b82
Mark Bonchi <markbonchi@gmail.com>
2022-10-18 18:24:01 -0700

    Add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+Composed of  function that adds two numbers
+
+"""
+
+def add_integer(a, b=98):
+    """
+    Function that adds two integers
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
