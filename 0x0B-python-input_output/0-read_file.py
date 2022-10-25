884a49d6855199875f2010d7a84d8294c7b49f54
Mark Bonchi <markbonchi@gmail.com>
2022-10-25 16:22:55 -0700

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Read file module"""
+def read_file(filename=""):
+    """
+    read_file
+
+    args:
+        filename: (str)
+    """
+    import os
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
