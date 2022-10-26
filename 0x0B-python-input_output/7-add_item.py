5e9a93ccf65f214284af066299ccd057c19b3948
Mark Bonchi <markbonchi@gmail.com>
2022-10-26 05:17:12 -0700

    Add item json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""add item module"""
+
+
+import json
+from sys import argv
+save_json = __import__("5-save_to_json_file").save_to_json_file
+load_json = __import__("6-load_from_json_file").load_from_json_file
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+    with open(filename, encoding="utf-8") as f:
+        try:
+            my_list = load_json(filename)
+        except Exception as err:
+            f.write(json.dumps([]))
+        else:
+            new_list = my_list + argv[1:]
+            save_json(new_list, filename)
