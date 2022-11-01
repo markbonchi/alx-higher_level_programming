76df622a1ee210b563c4d465e16c1719f9986c56
Mark Bonchi <markbonchi@gmail.com>
2022-10-31 20:29:40 -0700

    Update to Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
     Base class module
 """
+import json
 
 
 class Base:
@@ -13,6 +14,7 @@ class Base:
         id: (int) object id
     """
 
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -21,3 +23,66 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        converts to json string
+
+        args:
+            list_dictionaries: list of dictionaries
+        """
+        if list_dictionaries is None:
+            return json.dumps([])
+
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        converts from json string to data structure
+
+        args:
+            json_string: (str)
+        """
+        if json_string is None or json_string == "[]":
+            return []
+
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save_to_file
+        """
+        filename = "{}.json".format(cls.__name__)
+        my_list = [i.to_dictionary() for i in list_objs]
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(my_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+
+        args:
+            **dictionary: key/value pair of attributes to initiate
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        """
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                return [cls.create(**i) for i in cls.from_json_string(f.read())]
+        except FileNotFoundError:
+            return []
