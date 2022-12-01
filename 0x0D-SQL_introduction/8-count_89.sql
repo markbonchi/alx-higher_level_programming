c5387ecd7de5b74b0ccac38f804309f7b2b4789f
Mark Bonchi <markbonchi@gmail.com>
2022-11-30 16:00:01 -0800

    count record

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,5 @@
 -- Script that displays the number of records with id=89 in first_table
 -- Server name passed as argument
-SELECT COUNT(id=89) FROM first_table
+SELECT COUNT(*)
+FROM first_table
+WHERE id=89
