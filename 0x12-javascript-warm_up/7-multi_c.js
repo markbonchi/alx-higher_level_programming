14c68115ce81c1c46e0241b5d2e7d68cac2faac0
markbonchi <markbonchi@gmail.com>
2022-12-20 17:46:13 -0800

    Print times argument

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// process.argv
+const argv = process.argv;
+const num = parseInt(argv[2]);
+if (Number.isInteger(num)) {
+  for (let i = 0; i < num; i++) console.log('C is fun');
+} else {
+  console.log('Missing number of ocurrences');
+}
