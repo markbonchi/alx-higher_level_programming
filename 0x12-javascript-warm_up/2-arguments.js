c749f74b5cf64bf1a1fab358936b599a8ade417b
markbonchi <markbonchi@gmail.com>
2022-12-20 16:36:06 -0800

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,8 @@
 
 let i;
 process.argv.forEach((val, index) => { i = index; });
-i < 2 ? console.log('No argument') : console.log('Argument found');
+i > 2
+  ? console.log('Arguments found')
+  : i > 1
+    ? console.log('Argument found')
+    : console.log('No argument');
