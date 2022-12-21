89c893c85f28bf4e94373a2562b47b7e950c14eb
markbonchi <markbonchi@gmail.com>
2022-12-20 17:02:52 -0800

    Print sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // process.argv
 let i;
-let argv = process.argv;
+const argv = process.argv;
 argv.forEach((val, index) => { i = index; });
 if (i <= 3) {
-  let myVar = argv[2] + ' is ' + argv[3];
+  const myVar = argv[2] + ' is ' + argv[3];
   console.log(myVar);
 }
