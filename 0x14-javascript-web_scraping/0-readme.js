0b33389cd1e83f2f4e2a92c0d252ca9bc1f65985
markbonchi <markbonchi@gmail.com>
2023-02-10 21:39:30 +0000

    Fix to readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,10 +6,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (err, fd) => {
-  if (err){
+  if (err) {
     console.log(err);
   } else {
     console.log(fd);
   }
 });
-
