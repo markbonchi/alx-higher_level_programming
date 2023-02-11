79a6f7b796d0ceb2cb1a94f9c6b8cc4fbce5a88c
markbonchi <markbonchi@gmail.com>
2023-02-11 18:04:50 +0000

    Json todo

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else if (res.statusCode === 200) {
+    const bodyJSON = JSON.parse(body);
+    // console.log(bodyJSON);
+    var tempList = {};
+    for (const i in bodyJSON) {
+      const task = bodyJSON[i];
+      if (task.completed === true) {
+        if (tempList[task.userId] === undefined) {
+          tempList[task.userId] = 1;
+        } else {
+          tempList[task.userId]++;
+        }
+      }
+    }
+    console.log(tempList);
+  } else {
+    console.log(`Error code: ${res.statusCode}`)
+  }
+});
